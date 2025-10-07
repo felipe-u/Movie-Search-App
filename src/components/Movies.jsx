4bc2b5638c942a1239ab8929c7960a54ed3febde
Felipe U <felipe_uv@outlook.com>
2025-10-06 19:03:54 -0500

    Add sort button

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,3 +1,5 @@
+import { useMemo, useState } from 'react'
+
 export function Movies({ movies, hasSearched = false }) {
   return (
     <>
@@ -11,17 +13,32 @@ export function Movies({ movies, hasSearched = false }) {
 }
 
 function ListOfMovies({ movies }) {
+  const [sort, setSort] = useState(false)
+
+  const sortedMovies = useMemo(() => {
+    return sort
+      ? [...movies].sort((a, b) => a.Title.localeCompare(b.Title))
+      : movies
+  }, [sort, movies])
+
   return (
-    <div className='movies-container'>
-      {movies?.map((movie) => {
-        return (
-          <div className='movie-container' key={movie.imdbID}>
-            <img src={movie.Poster} alt={`${movie.Title} poster from OMDB`} />
-            <h3>{movie.Title}</h3>
-            <p>{movie.Year}</p>
-          </div>
-        )
-      })}
-    </div>
+    <>
+      <div className='sort-btn-container'>
+        <button onClick={() => setSort(!sort)} className='primary-btn sort-btn'>
+          Sort
+        </button>
+      </div>
+      <div className='movies-container'>
+        {sortedMovies?.map((movie) => {
+          return (
+            <div className='movie-container' key={movie.imdbID}>
+              <img src={movie.Poster} alt={`${movie.Title} poster from OMDB`} />
+              <h3>{movie.Title}</h3>
+              <p>{movie.Year}</p>
+            </div>
+          )
+        })}
+      </div>
+    </>
   )
 }
